fa99e0f4638d6c22f9832420c6e2bda65ebc82bd
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2025-07-14 18:58:25 -0400

    legendsviewer-next: move tests to installCheckPhase

--- pkgs/by-name/le/legendsviewer-next/package.nix
+++ pkgs/by-name/le/legendsviewer-next/package.nix
@@ -84,8 +84,24 @@ stdenv.mkDerivation rec {
     runHook postInstall
   '';
 
+  doInstallCheck = true;
+
+  installCheckPhase = ''
+    runHook preInstallCheck
+
+    timeout 10 $out/bin/LegendsViewer &
+    sleep 5
+
+    # Static server is up
+    curl -f http://localhost:8081 | grep "<!doctype html>"
+
+    # Version matches expected
+    curl -f http://localhost:5054/api/version | grep ${version}
+
+    runHook postInstallCheck
+  '';
+
   passthru = {
-    tests.tests = callPackage ./tests.nix { };
     updateScript = nix-update-script { };
   };
 
